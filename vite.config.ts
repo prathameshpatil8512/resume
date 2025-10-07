bf60ef48d85957c9357614a9af59b677d00674f0
prathameshpatil8512 <prathameshpatil8512@yahoo.com>
2025-10-07 11:58:24 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/resume/',
-})
+  plugins: [react()],
+  base: '/resume/', // important for GitHub Pages
+});
 
