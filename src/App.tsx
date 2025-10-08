642c5ad044c4acb19e8b69ef7a7da12376957cf9
prathameshpatil8512 <prathameshpatil8512@yahoo.com>
2025-10-08 10:06:41 +0530

    email

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ function App() {
       <section id="contact" className="contact">
         <h2>Get In Touch</h2>
         <p>Interested in working together or just want to say hi?</p>
-        <a href="mailto:prathameshpatil8512.com" className="cta">Contact Me</a>
+        <a href="prathameshpatil8512.com" className="cta">Contact Me</a>
       </section>
 
       {/* Footer */}
